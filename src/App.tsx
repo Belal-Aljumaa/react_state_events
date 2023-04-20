e85a3781a3ca1e5cde0e25c46ee00bc85ab15efa
Belal-Aljumaa <bilal.joma90@gmail.com>
2023-04-20 15:26:03 +0200

    create books json file and styled the site

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,26 @@
-import './App.css'
+import './App.scss';
+import books from './data/books.json';
 
 function App() {
   return (
     <div>
+      <h1>Book Site</h1>
+      <h2>There are {books.length} Books:</h2>
+      <div className="books">
+        {books.map((book) => {
+          return (
+            <div className="book" key={book.id}>
+              <img className="cover" src={book.imageUrl} />
+              <div className="info">
+                <div className="title">{book.title}</div>
+                <div className="author">{book.author}</div>
+              </div>
+            </div>
+          );
+        })}
+      </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
